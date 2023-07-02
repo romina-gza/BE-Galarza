027484583092b59247075831cc7cc4fcae8be258
Romina Galarza <romina.celeste.gza@gmail.com>
2023-07-01 23:51:20 -0300

    more nothing

--- PPGalarza/src/app.js
+++ PPGalarza/src/app.js
@@ -1,1 +1,16 @@
+import express from "express";
 import { Router } from "express";
+import multer from 'multer'
+
+const app = express()
+
+app.use(express.static('./src/public'))
+
+const storage = multer.diskStorage({
+    destination: function (res, file, callback) {
+        callback(null, './src/public/thumbnail')
+    },
+    filename: function (req, file, callback) {
+        callback(null, new Date().getTime() + file.originalname)
+    } // 02:27:42
+})
